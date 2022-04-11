c12819ad335e87a8ea315290f31ed8cb922b2c57
Ryan-Shiu <ryan.shiu23@ycdsbk12.ca>
2022-04-10 23:08:58 -0400

    Updated finished task

--- Sketch.java
+++ Sketch.java
@@ -35,6 +35,7 @@ public class Sketch extends PApplet {
 
   }
   /**
+   * Method to draw the grass
    * 
    * @param grassX  x value for the grass
    * @param grassY  y value for the grass
@@ -43,14 +44,15 @@ public class Sketch extends PApplet {
    * @param grassColour1 the first colour for the grass in rgb
    * @param grassColour2 the second colour for the grass in rgb
    * @param grassColour3 the third colour for the grass in rgb
+   * 
    */
-  // Method to draw the grass
   public void drawGrass(float grassX, float grassY, float grassWidth, float grassHeight, float grassColour1,float grassColour2, float grassColour3){
     fill(grassColour1, grassColour2, grassColour3);
     rect(grassX, grassY, grassWidth, grassHeight);
   }
 
   /**
+   * Method to draw the house
    * 
    * @param houseX x value for the house
    * @param houseY y value for the house
@@ -59,8 +61,8 @@ public class Sketch extends PApplet {
    * @param houseColour1 the first colour for the house in rgb
    * @param houseColour2 the second colour for the house in rgb
    * @param houseColour3 the thrid colour for the house in rgb
+   * 
    */
-  // Method to draw the house
   public void drawHouse(float houseX, float houseY, float houseBase, float houseHeight, float houseColour1, float houseColour2, float houseColour3){
     // Code for the rectangle of the house
     fill(houseColour1, houseColour2, houseColour3);
@@ -72,6 +74,7 @@ public class Sketch extends PApplet {
   }
 
   /**
+   * Method to draw the windows
    * 
    * @param windowX x value for the windows
    * @param windowY y value for the windows
@@ -80,8 +83,8 @@ public class Sketch extends PApplet {
    * @param windowColour1 the red for the windows in rgb
    * @param windowColour2 the green for the windows in rgb
    * @param windowColour3 the blue for the windows in rgb
+   * 
    */
-  // Method to draw the windows
   public void drawWindows(float windowX, float windowY, float windowWidth, float windowHeight, float windowColour1, float windowColour2, float windowColour3){
     // Right window
     fill(windowColour1, windowColour2, windowColour3);
@@ -99,6 +102,7 @@ public class Sketch extends PApplet {
   }
 
   /**
+   * Method to draw the door
    * 
    * @param doorX x value for the door
    * @param doorY y value for the door
@@ -107,14 +111,15 @@ public class Sketch extends PApplet {
    * @param doorColour1 the red for the door in rgb
    * @param doorColour2 the green for the door in rgb
    * @param doorColour3 the blue for the door in rgb
+   * 
    */
-  // Method to draw the door
   public void drawdoor(float doorX, float doorY, float doorWidth, float doorHeight, float doorColour1, float doorColour2, float doorColour3){
     fill(doorColour1, doorColour2, doorColour3);
     rect(doorX, doorY, doorWidth, doorHeight);
   }
   
   /**
+   * Method to draw the chimney
    * 
    * @param chimneyX x value for the chimney
    * @param chimneyY y value for the chimney
@@ -123,14 +128,15 @@ public class Sketch extends PApplet {
    * @param chimneyColour1 the red for the chimney in rgb
    * @param chimneyColour2 the green for the chimney in rgb
    * @param chimneyColour3 the blue for the chminey in rgb
+   * 
    */
-  // Method to draw the chimney
   public void drawChimney(float chimneyX, float chimneyY, float chimneyWidth, float chimneyHeight, float chimneyColour1, float chimneyColour2, float chimneyColour3){
     fill(chimneyColour1, chimneyColour2, chimneyColour3);
     rect(chimneyX, chimneyY, chimneyWidth, chimneyHeight);
   }
 
   /**
+   * Method to draw the sun
    * 
    * @param sunX x value for the sun
    * @param sunY y value for the sun
@@ -139,15 +145,15 @@ public class Sketch extends PApplet {
    * @param sunColour1 the red for the sun in rgb
    * @param sunColour2 the green for the sun in rgb
    * @param sunColour3 the blue for the sun in rgb
+   * 
    */
-  // Method to draw the sun
   public void drawSun(float sunX, float sunY, float sunWidth, float sunHeight, float sunColour1, float sunColour2, float sunColour3){
     fill(sunColour1, sunColour2, sunColour3);
     ellipse(sunX, sunY, sunWidth, sunWidth);
   }
 
   /**
-   * 
+   * Method to draw the flower
    * @param flowerX initial x value for the flower
    * @param flowerY initial y value for the flower
    * @param flowerWidth the initial width value for the flower
@@ -158,8 +164,8 @@ public class Sketch extends PApplet {
    * @param flowerAngle the angle that the flower pedals will rotate in the loop
    * @param translateX the x value that will be translated in order to rotate the pedals in the same position
    * @param translateY the y value that will be translated in order to rotate the pedals in the same position
+   * 
    */
-  // Method to draw the flower
   public void drawFlower(float flowerX, float flowerY, float flowerWidth, float flowerHeight, float flowerColour1, float flowerColour2, float flowerColour3, float flowerAngle, float translateX, float translateY){
     // Code to draw the flower stem
     fill(flowerColour1 - 236, flowerColour2 + 209, flowerColour3 - 166);
